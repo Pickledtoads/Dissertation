772d7eac359c6b93b38b08b9cc5f2eec8424951b
PickledToads <julu.lund@gmail.com>
2020-02-17 16:30:03 +0000

    Wrote a script to centrally launch the IBM models.

---
+++ RunAlgorithms.jl
@@ -0,0 +1,59 @@
+
+# Import all required packages
+using HDF5, JLD, Base.Threads
+
+French = readlines(joinpath(@__DIR__,"CleanedShortFrench.txt"))
+English = readlines(joinpath(@__DIR__,"CleanedShortEnglish.txt"))
+n = 2000
+
+include(joinpath(@__DIR__, "Scripts\\UtilityFunctions.jl"))
+include(joinpath(@__DIR__, "Scripts\\IBM_Initialize.jl"))
+
+# Run the model that initializes the IBM models
+Name1 = string(joinpath(@__DIR__, "Trained\\"),"IBM_init_", n, ".jld")
+# if this model has already been trained load in the data
+if isfile(Name1)
+    IBM_init = load(Name1)["IBM_init"]
+    println("loaded - IBM_init")
+else
+    IBM_init = Initialize(English[1:n], French[1:n])
+    save(Name1, "IBM_init", IBM_init)
+end
+
+# Run the first IBM Model
+include(joinpath(@__DIR__, "Scripts\\IBM1.jl"))
+Name2 = string(joinpath(@__DIR__, "Trained\\"),"IBM1_save_", n, ".jld")
+
+if isfile(Name2)
+    IBM1_save = load(Name2)["IBM1_save"]
+    println("loaded - IBM1_save" )
+else
+    IBM1_save = IBM1(English[1:n], French[1:n], 25, IBM_init)
+    save(Name2, "IBM1_save", IBM1_save)
+end
+
+# Run the second IBM Model
+include(joinpath(@__DIR__, "Scripts\\IBM2.jl"))
+Name3 = string(joinpath(@__DIR__, "Trained\\"),"IBM2_save_", n, ".jld")
+
+if isfile(Name3)
+    IBM2_save_ = load(Name1)["IBM2_save"]
+    println("loaded - IBM2_save" )
+else
+    IBM2_save = IBM2(English[1:n], French[1:n], 25, IBM1_save)
+    save(Name3, "IBM2_save", IBM2_save)
+end
+
+
+# Now do IBM3
+include(joinpath(@__DIR__, "Scripts\\IBM3.jl"))
+Name4 = string(joinpath(@__DIR__, "Trained\\"),"IBM3_save_", n, ".jld")
+
+if isfile(Name4)
+    IBM3_save_ = load(Name1)["IBM3_save"]
+    println("loaded - IBM3_save" )
+
+else
+    IBM3_save = IBM3(English[1:n], French[1:n], 5, IBM2_save)
+    save(Name4, "IBM3_save", IBM3_save)
+end
