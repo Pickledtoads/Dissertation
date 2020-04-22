63531d69ae33a7363518758d70ca996d8dacdc8c
Pickledtoads <43879355+Pickledtoads@users.noreply.github.com>
2020-04-22 14:10:03 +0100

    Finalised app

--- Translation_Shiny/app.R
+++ Translation_Shiny/app.R
@@ -14,17 +14,17 @@ source("Shift_to_R.R")
 # Define UI for application that applies the IBM translation models
 ui <- fluidPage(
     shinyjs::useShinyjs(),
-    
+
     # Application title
     titlePanel("French-English Translation Using the IBM Models"),
 
-    # Dropdown to select which model you wish to use for translation 
+    # Dropdown to select which model you wish to use for translation
     sidebarLayout(position = "left",
         sidebarPanel(width=4,
             textInput("input_File",value ="C:/Users/julul/github/Dissertation/Trained/", "Input the path to the folder with trained distributions:"),
             selectInput("ModelChoice","Which model to use for translation:",c("IBM 1","IBM 2","IBM 3","IBM 4","IBM 5")),
         ),
-        
+
         # boxes to input the desired phrase for translation
         # And to view the output
         mainPanel(width=8,
@@ -38,60 +38,86 @@ ui <- fluidPage(
                   actionButton("BLEU_Button", "Press to Calculate BLEU"),
                   br(),
                   br(),
-                  textOutput("BLEU_Box")
-                  
-                  
+                  textOutput("BLEU_Box"),
+                  br(),
+                  textAreaInput(inputId="ref_A_Box", value = 50,label = "How many sentences do you want to average BLEU across?", resize="none"),
+                  actionButton("BLEU_A_Button", "Press to Calculate Average BLEU"),
+                  br(),
+                  br(),
+                  textOutput("BLEU_A_Box")
+
+
         )
     )
-    
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
+
   # prevent user from entering data in the output box
   shinyjs::disable("output_Box")
   rResult <- reactiveValues(translation = "")
   observeEvent(input$trans_Button, {
-    
+
     # read in the inputs from textboxes
     folder <- input$input_File
     model <- input$ModelChoice
     french <- unlist(strsplit(input$input_Box,split = " "))
-    
+
     # use the dropdown to select the right model
     # then run the model to translate
     if (model == "IBM 1"){
-      translation <- IBM1_Translate(french, 10000)
+      translation <- IBM1_Translate(french, 10000, folder)
       output$output_Box <- renderText({paste(translation)})
     }
     else if (model == "IBM 2"){
-      translation <- IBM2_Translate(french, 10000)
+      translation <- IBM2_Translate(french, 10000,folder)
       output$output_Box <- renderText({paste(translation)})
     }
     else if (model == "IBM 3"){
-      translation <- IBM3_Translate(french, 10000)
+      translation <- IBM3_Translate(french, 10000,folder)
       output$output_Box <- renderText({paste(translation)})
     }
     else if (model == "IBM 4"){
-      translation <- IBM4_Translate(french, 10000)
+      translation <- IBM4_Translate(french, 10000,folder)
       output$output_Box <- renderText({paste(translation)})
     }
-    
+    else if (model == "IBM 5"){
+      translation <- IBM5_Translate(french, 10000,folder)
+      output$output_Box <- renderText({paste(translation)})
+    }
+
     # pass the translation result to the reactive
     rResult$translation <- translation
-    
+
   })
-  
+
   # button to calculate the BLEU
   observeEvent(input$BLEU_Button, {
     reference <- input$ref_Box
     val <- BLEU(reference,rResult$translation)
     output$BLEU_Box <- renderText({paste(val)})
-    
+
   })
-   
+
+  observeEvent(input$BLEU_A_Button, {
+
+    source("Corpus_BLEU.R")
+    model <- input$ModelChoice
+
+
+    # choose the correct filepaths for the corpus
+    path <- as.character(input$input_File)
+    folder <- path
+    path <- substr(path,1,(nchar(path)-8))
+    Fre_path <- paste(path,"CleanedShortFrench.txt", sep="")
+    Eng_path <- paste(path,"CleanedShortEnglish.txt", sep="")
+    BLEU <- Corpus_BLEU(Fre_path, Eng_path, model, input$ref_A_Box, folder)
+    output$BLEU_A_Box <- renderText({paste(BLEU)})
+
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
